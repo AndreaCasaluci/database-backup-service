faba8fa09a4de54343a5f6636d27f25d6734f868
Andrea Casaluci <andrea@MBP-di-Andrea.lan>
2025-07-03 11:53:46 +0200

    fix: give permission to user for mounted volume

--- build.sh
+++ build.sh
@@ -3,7 +3,17 @@
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 
+# Load variables from .env
+set -a
+source .env
+set +a
+
 echo "Building with USER_ID=${USER_ID} and GROUP_ID=${GROUP_ID}"
+echo "Using BACKUP_DIR=${BACKUP_DIR}"
+
+# Ensure backup dir exists and is writable
+mkdir -p "$BACKUP_DIR"
+sudo chown -R ${USER_ID}:${GROUP_ID} "$BACKUP_DIR"
 
 docker compose build --no-cache
 docker compose up -d
